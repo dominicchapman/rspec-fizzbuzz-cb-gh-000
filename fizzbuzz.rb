fabf0afad929bea762b366935f3b55ca6dc777f5
Dominic Chapman <dominicchapman@me.com>
2017-09-24 21:24:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(argument)
-  if argument % 3 == 0
-    return "Fizz"
+  if (argument % 3 == 0) && (argument % 5 == 0)
+    return "FizzBuzz"
   elsif argument % 5 == 0
     return "Buzz"
-  elsif argument % 15 == 0
-    return "FizzBuzz"
+  elsif argument % 3 == 0
+    return "Fizz"
   elsif argument % 4 == 0
     return nil
   else
